6e02f7669d631dd7babad482a00961dd49022a94
Cupid0nn <malaucierno@gmail.com>
2025-02-18 14:08:44 -0300

    dia #1

---
+++ promediate.js
@@ -0,0 +1,18 @@
+//make a function that takes an array of numbers and returns the average of the numbers, even with commas
+// crea una funcion que tome un array de numeros y devuelva el promedio de los numeros, incluso con coma
+
+// pasos para realizar la tarea
+// declarar variable
+// recorrer el array
+// sumar los numeros
+// dividir la suma entre la longitud del array o lo que es lo mismo divir la suma por la cantidad de numeros en el array
+
+function average(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum / arr.length;
+  }
+
+  console.log(average([8]));
